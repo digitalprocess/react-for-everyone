61581a7b8fb5772fe27cfd5936c0a65f4a147c59
frontendista <csdelreal@gmail.com>
2020-12-02 20:56:01 -0600

    styled components, api calls, miscellaneous

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function Movie({movie}) {
+export default function Movie({ movie, config: { images } }) {
 	return (
-		<li>{movie.name}</li>
+		<li>
+			{images &&
+				<img
+					src={images.base_url + images.poster_sizes[3]  + movie.poster_path}
+					alt={movie.title + ' Poster'}
+				/>
+			}
+			<h3>{movie.title}</h3>
+		</li>
 	)
 }
 
 Movie.propTypes = {
 	movie: PropTypes.shape({
-		name: PropTypes.string
+		title: PropTypes.string.isRequired,
+		poster_path: PropTypes.string.isRequired,
 	})
 }
--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -1,30 +1,64 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 import Filter from '../Filter'
 import Movie from './Movie'
 
-const movies = [
-	{
-		name: 'Shawshank Redemption'
-	},
-	{
-		name: 'The Usual Suspects'
-	},
-	{
-		name: 'Best In Show'
-	},
-]
+const MOVIE_API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+const MOVIE_CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${process.env.REACT_APP_API_KEY}
+`
 
 export default function MoviesList() {
 	const [filter, setFilter] = useState('')
+	const [movies, setMovies] = useState([])
+	const [config, setConfig] = useState({})
+
+	const getMovies = async () => {
+		try {
+			const res = await fetch(MOVIE_API_URL)
+			const movies = await res.json()
+			setMovies(movies.results)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const getConfig = async () => {
+		try {
+			const res = await fetch(MOVIE_CONFIG_URL)
+			const config = await res.json()
+			setConfig(config)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		getMovies()
+		getConfig()
+	}, [])
+
 	return (
 		<div>
 			<Filter filter={filter} setFilter={setFilter} />
-			<ul>
+			<MoviesListStyles>
 				{movies
-					.filter(movie => movie.name.toLowerCase().includes(filter.toLocaleLowerCase()))
-					.map(movie => <Movie key={movie.name} movie={movie} />)
+					.filter(movie => movie.title.toLowerCase().includes(filter.toLocaleLowerCase()))
+					.map(movie => <Movie key={movie.id} config={config} movie={movie} />)
 				}
-			</ul>
+			</MoviesListStyles>
 		</div>
 	)
 }
+
+const MoviesListStyles = styled.ul`
+	list-style: none;
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 1rem;
+	margin: 0;
+	padding: 1rem;
+	img {
+		width: 100%;
+		box-shadow: 1px 1px 10px rgba( 0, 0, 0, 0.4)
+	}
+`
